2a503f5e6f1bd40c0893d9b05780e7d1f21f2a08
Minhaj Ahmed Moin <mampk98@users.noreply.github.com>
2018-04-12 23:41:02 +0500

    First Commit

---
+++ Project Sounds/myui.m
@@ -0,0 +1,16 @@
+%%
+function myui
+    f = figure;
+    myData = { 'A '  31; 'B'  41; 'C'  5; 'D' 2.6};
+    t = uitable('Parent',f,...
+                'Position', [25 25 700 200], ...
+                'Data',myData,...
+                'ColumnEditable', [false true], ...
+                'CellEditCallback',@converttonum);
+        function converttonum(hObject,callbackdata)
+             numval = eval(callbackdata.EditData);
+             r = callbackdata.Indices(1)
+             c = callbackdata.Indices(2)
+             hObject.Data{r,c} = numval; 
+        end
+end
